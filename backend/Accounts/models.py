01bb32802dc92204b85d9ce0f68dfea8e8b43c94
Nuo Xu <nnuoxu@gmail.com>
2022-11-17 15:04:20 -0500

    token auth almost done

--- backend/Accounts/models.py
+++ backend/Accounts/models.py
@@ -5,6 +5,6 @@ from phone_field import PhoneField
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
     phone_number = PhoneField(blank=True, help_text='Contact phone number')
-    # avatar = models.ImageField()
+    avatar = models.ImageField(blank=True, null=True, upload_to="images/profile")
     # todo: subscription
     # todo: payment method 
--- backend/Accounts/views.py
+++ backend/Accounts/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 import rest_framework
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import AuthenticationFailed
-from .serializers import SignUpSerializer
-from .models import User
+from .serializers import SignUpSerializer, RetrieveUpdateProfileSerializer
+from .models import Profile
+from django.contrib.auth.models import User
 import jwt, datetime
 
 
@@ -22,36 +25,58 @@ class RegisterView(APIView):
 
 
 class LogInView(APIView):
+    permission_classes = []
     def post(self,request):
         username = request.data['username']
         password = request.data['password']
 
-        user = User.objects.filter(username = username).first()
+        user = authenticate(username=username, password=password)
 
-        # if user is None:
-        #     raise AuthenticationFailed('User not found')
+        if user is None:
+            raise AuthenticationFailed('Incorrent Username or Password')
         
-        # if not user.check_password(password):
-        #     raise AuthenticationFailed('Incorrect Password')
-        
-        token, created = Token.objects.get_or_create(user=user)
-        return rest_framework.response.Response({"token": token.key})
+        response = {"message": "Login Successfull", "tokens": user.auth_token.key}
+        return Response(data=response)
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 
-# class UserView(APIView):
-#     def get(self,request):
-#         token = request.COOKIES.get('jwt')
+# class ProfileAPI(APIView):
+#     permission_classes = [IsAuthenticated]
+#     def get(self, request, *args, **kwargs):
+#         user = get_object_or_404(User, pk=kwargs['user_id'])
+#         profile = Profile.objects.get(user=user)
+#         print(profile)
+#         profile_serializer = RestrictedUserSerializer(profile.user)
+#         return Response(profile_serializer.data)
+        
+#     def post(self, request, *args, **kwargs):
+#         user = get_object_or_404(User, pk=kwargs['user_id'])
 
-#         if not token:
-#             raise AuthenticationFailed('Unauthenticated')
-        
-#         try:
-#             payload = jwt.decode(token, 'secret', algorithms=['HS256'])
-        
-#         except jwt.ExpiredSignatureError:
-#             raise AuthenticationFailed('Unauthenticated')
-        
-#         user = User.objects.filter(id=payload['id']).first()
-#         serializer = UserSerializer(user)
+class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = RetrieveUpdateProfileSerializer
 
-#         return Response(serializer.data)
+    def retrieve(self, request, *args, **kwargs):
+        user = get_object_or_404(User, pk=kwargs['user_id'])
+        profile = Profile.objects.get(user=user)
+        profile_serializer = RetrieveUpdateProfileSerializer(profile.user)
+        return Response(profile_serializer.data)
+    
+    def perform_update(self, serializer):
+        return serializer.save()
+    
+    def update(self, request, *args, **kwargs):
+        user = get_object_or_404(User, pk=kwargs['user_id'])
+        profile = Profile.objects.get(user=user)
+        serializer = RetrieveUpdateProfileSerializer(profile.user, data=request.data)
+        
+        serializer.is_valid(raise_exception=True)
+        print("here")
+        print(serializer.data)
+        instance = serializer.update(instance=profile, validated_data=request.data)
+        print(instance)
+        serializer = RetrieveUpdateProfileSerializer(instance.user)
+        return Response(serializer.data)
