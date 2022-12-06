3d09a97d2fbefa626b8430693e1d45d764602cbd
NoraXu111 <92701302+NoraXu111@users.noreply.github.com>
2022-12-05 20:19:55 -0500

    Delete frontend/src/components/Text directory

--- frontend/src/components/Text/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Text extends React.Component {
-    render(){
-        return <h1 style={{color: 'red'}}>{this.props.children}<br/></h1>
-    }
-}
-
-export default Text;
