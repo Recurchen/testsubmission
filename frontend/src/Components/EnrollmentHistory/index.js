d1551d28be41ce65cb6a600d81dbef7140921370
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2022-12-07 16:50:29 -0500

    add token for fetch in enrol/drop class and see enrollmentHistory

--- frontend/src/Components/EnrollmentHistory/index.js
+++ frontend/src/Components/EnrollmentHistory/index.js
@@ -1,8 +1,15 @@
 import {useContext, useEffect, useState} from "react";
 import EnrollmentHistoryTable from "./EnrollmentHistoryTable";
 import EnrollmentHistoryAPIContext from "../../Contexts/EnrollmentHistoryAPIContext";
+import useToken from "../../useToken";
+import {useNavigate} from "react-router-dom";
 
 const EnrollmentHistory = () => {
+    const token = useToken();
+    const navigate = useNavigate();
+    const toLogin = ()=>{
+        navigate('/login');
+    }
     const perPage = 10;
     const [params, setParams] = useState({page: 1})
     const { setEnrollmentHistory } = useContext(EnrollmentHistoryAPIContext);
@@ -10,13 +17,25 @@ const EnrollmentHistory = () => {
     const [hasPrev, setHasPrev] = useState(false)
     useEffect(() => {
         const { page } = params;
-        fetch(`http://localhost:8000/classes/enrollments/?page=${page}&per_page=${perPage}`)
+        if(token.token === null){
+            toLogin();
+        }
+        else{
+            fetch(`http://localhost:8000/classes/enrollments/?page=${page}&per_page=${perPage}`,
+                {headers: new Headers({
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token.token}`
+                    })}).then(res=>{
+                        if(res.status ==='301'){toLogin();}
+                        return res;
+            })
             .then(res => res.json())
             .then(json => {
                 setEnrollmentHistory(json.results);
                 json.next?setHasNext(true):setHasNext(false);
                 json.previous?setHasPrev(true):setHasPrev(false);
             })
+        }
     }, [params])
 
     return (
