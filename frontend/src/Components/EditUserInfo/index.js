7f1c7921711bd25e4d6298ebd220504b404a05b0
Nuo Xu <nnuoxu@gmail.com>
2022-12-08 02:02:06 -0500

    add payment method need to fix

--- frontend/src/Components/EditUserInfo/index.js
+++ frontend/src/Components/EditUserInfo/index.js
@@ -2,71 +2,59 @@ import React, { useState } from "react";
 import './style.css';
 import {Link, Outlet} from "react-router-dom";
 import PopUp from '../Popup'; 
-import Header from '../header';
-import Footer from '../footer';
+import useToken from "../../useToken";
+import useUserId from "../../useUserId";
+import { useNavigate } from "react-router-dom";
 
 const EditUserInfo = (props) => {
-    const [username, setUsername] = useState('');
+
+    const token = useToken();
+    const id = useUserId();
+
+    const userDetailHeaders = new Headers({
+        'Authorization' : `Bearer ${token.token}`
+    })
+
     const [firstname, setFirstname] = useState('');
     const [lastname, setLastname] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [avatar, setAvatar] = useState(null);
-    const [pass1, setPass1] = useState('');
-    const [success, setSuccess] = useState(false);
 
-    function showSuccess(){
-       setSuccess(!success);
-       console.log("here");
+    const navigate = useNavigate();
+    const navToUserCenter = ()=>{
+        navigate('/usercenter', {})
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(avatar);
         const formData = new FormData();
-        formData.append("username", `${username}`);
-        formData.append("password", `${pass1}`);
-        formData.append("password2", `${pass2}`);
         formData.append("first_name", `${firstname}`);
         formData.append("last_name", `${lastname}`);
         formData.append("email", `${email}`);
         formData.append("phone_number", `${phone}`);
         formData.append('avatar', avatar);
 
-       //  for (const pair of formData.entries()){
-       //        console.log(`${pair[0]},${pair[1]}`);
-       //  }
+       console.log(formData.values());
 
-
-        fetch('http://127.0.0.1:8000/accounts/register/', {
-            method: 'POST',
+        fetch(`http://127.0.0.1:8000/accounts/users/${id.userId}/detail/`, {
+            method: 'PUT',
             body: formData,
-            redirect: "follow" })
+            headers: userDetailHeaders})
             .then(res=>{
+                console.log(res);
               if(res.status === 200){
-                     showSuccess();
+                     navToUserCenter();
               }
             })
-       //      .then(res=>res.json())
-       //      .then(json => console.log(json))
-            // TODO: redirect
     }
 
     return (
-       <div>
-       < Header />
         <div className="auth-form-container">
-            <h2 className="log-reg-title" >Register</h2>
+            <h2 className="log-reg-title" >Change Your Personal Info Here</h2>
             <div>
-              {success ? <PopUp /> : null}
               </div>
         <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="username">Username</label>
-            <input value={username} 
-                   onChange={(e) => setUsername(e.target.value)}
-                   name="username" 
-                   id="username" 
-                   placeholder="enter your username" />
             <label htmlFor="email">Email</label>
             <input value={email} 
                    onChange={(e) => setEmail(e.target.value)}
@@ -101,25 +89,8 @@ const EditUserInfo = (props) => {
                    id="avatar" 
                    name="avatar"
                    accept="image/png, image/jpeg" />           
-            <label htmlFor="pass1">Password</label>
-            <input value={pass1} 
-                   onChange={(e) => setPass1(e.target.value)} 
-                   type="password" 
-                   placeholder="enter your password" 
-                   id="pass1" 
-                   name="pass1" />
-            <label htmlFor="pass1">Confirm Password</label>
-            <input value={pass2} 
-                   onChange={(e) => setPass2(e.target.value)} 
-                   type="password" 
-                   placeholder="confirm your password" 
-                   id="pass2" 
-                   name="pass2" />
-            <button className="reg-log-btn" id="submit" type="submit">Sign Up</button>
+            <button className="reg-log-btn" id="submit" type="submit">Update</button>
         </form>
-        <Link to="/login" className="link-btn">Already have an account? Login here.</Link>
-    </div>
-    <Footer />
     </div>
     )
 }
