183aa70b0deaf291ebf618c1e279902ec7884b65
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2022-12-10 00:45:37 -0500

    update

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ python3 -m venv venv
 cd ..
 cd frontend
 npm install
-npm install @material-ui/core --legacy-peer-deps
 npm install recompose --legacy-peer-deps
 npm install react-google-maps --legacy-peer-deps
+npm install @material-ui/core --legacy-peer-deps
 cd ..
